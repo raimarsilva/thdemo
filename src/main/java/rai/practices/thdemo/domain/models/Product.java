41caa696745e190efabbb9b4ddb8a1a59625e665
raimarsilva <106407456+raimarsilva@users.noreply.github.com>
2025-03-25 14:33:14 -0300

    feat: added mongodb config and hibernate config.

--- src/main/java/rai/practices/thdemo/domain/models/Product.java
+++ src/main/java/rai/practices/thdemo/domain/models/Product.java
@@ -2,11 +2,12 @@ package rai.practices.thdemo.domain.models;
 
 import java.util.List;
 
-//import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "products")
 public record Product(
-                // @Id
-                Long id,
+                @Id Long id,
                 String name,
                 double price,
                 boolean inStock,
